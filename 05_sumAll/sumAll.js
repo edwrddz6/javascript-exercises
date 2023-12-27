26fa78eba6dd8f0cb59fd9e1d8e5f8d96252780f
Edward Diaz <diazedward24@gmail.com>
2023-12-27 10:06:30 -0800

    Add solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -4,3 +4,5 @@ const sumAll = function() {
 
 // Do not edit below this line
 module.exports = sumAll;
+
+// Implement a function that takes 2 integers and returns the sum of every number between(and including) them
